74d49a4e09065d8a7d9cbf45bac95e1d2682dc88
Lars Melchior <TheLartians@users.noreply.github.com>
2020-03-21 16:20:27 +0100

    add react-simple-chatbot

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ChatBot from "react-simple-chatbot";
+
+const steps = [
+  {
+    id: "1",
+    message: "What is your name?",
+    trigger: "2",
+  },
+  {
+    id: "2",
+    trigger: "3",
+    user: true,
+  },
+  {
+    end: true,
+    id: "3",
+    message: "Hi {previousValue}, nice to meet you!",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div style={{backgroundColor: "gray", width: "100%", minHeight: "100vh", display: "flex", flexDirection: "column"}}>
+      <div style={{display: "flex", flex: 1, flexDirection: "column", justifyContent: "center", alignSelf: "center"}}>
+        <div style={{flex: 1}}/>
+        <p style={{textAlign: "center", fontWeight: "bold", color: "white", fontSize: 25}}>
+          Chatbot test
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+        <ChatBot steps={steps} />
+        </div>
+        <div style={{flex: 1}}/>
+      </div>
     </div>
   );
 }
