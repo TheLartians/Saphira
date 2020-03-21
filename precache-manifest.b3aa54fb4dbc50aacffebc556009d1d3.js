5aed4a0a36c8176f75beb9587438060a0afae4c2
Lars Melchior <TheLartians@users.noreply.github.com>
2020-03-21 16:26:18 +0100

    Updates

---
+++ precache-manifest.b3aa54fb4dbc50aacffebc556009d1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a01bf828467c53aa56a942ba43db114",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e75fc3f5040cd978659a",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2d76603a7af024afc76",
+    "url": "./static/js/2.eda170bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.eda170bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75fc3f5040cd978659a",
+    "url": "./static/js/main.a619a1c5.chunk.js"
+  },
+  {
+    "revision": "e4a5dd60035ca01b707a",
+    "url": "./static/js/runtime-main.dcd788c3.js"
+  }
+]);
