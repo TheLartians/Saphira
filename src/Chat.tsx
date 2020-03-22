9c6149988af053c17d9b0c53789dbcc73f6f2d27
André Kovac <me@andrekovac.com>
2020-03-22 11:44:56 +0100

    Add first few final steps + adjust code

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -7,6 +7,8 @@ import { ChatBotAction } from './state/action';
 import { Profile } from './Profile';
 import { CodeBlock } from './CodeBlock';
 
+type TriggerProps = { value: string; steps?: any };
+
 export function ProgrammingChatBot() {
   const dispatch = useDispatch<Dispatch<ChatBotAction>>();
   const [height, setHeight] = useState(window.innerHeight);
@@ -20,6 +22,153 @@ export function ProgrammingChatBot() {
   });
 
   const steps = [
+    {
+      id: 'start',
+      message: 'Hej, ich bin Saphira 🐉 Wie heißt du? 🤗',
+      trigger: 'enter-username',
+    },
+    {
+      id: 'enter-username',
+      user: true,
+      trigger: 'ask-learn',
+    },
+    {
+      id: 'ask-learn',
+      message: (args: MessageArgs) => {
+        dispatch({ type: 'setName', value: args.previousValue });
+        return `${args.previousValue}, möchtest du lernen, wie man ganz einfach eine Homepage programmiert? 👩💻👨💻`;
+      },
+      trigger: 'select-learn',
+    },
+    {
+      id: 'select-learn',
+      options: [
+        { value: 1, label: 'Ja', trigger: 'learn-yes' },
+        { value: 2, label: 'Nein', trigger: 'learn-no' },
+      ],
+    },
+    {
+      id: 'learn-yes',
+      message: 'Super, dann legen wir direkt los! 🤩',
+      trigger: 'only-smartphone',
+    },
+    {
+      id: 'learn-no',
+      message: 'Schade. Vielleicht kann ich dich dennoch überzeugen 🤔',
+      trigger: 'only-smartphone',
+    },
+    {
+      id: 'only-smartphone',
+      message: 'Dazu brauchst du nichts weiter als dein Smartphone... 🤳',
+      trigger: 'explain-build-together',
+      delay: 2000,
+    },
+    {
+      id: 'explain-build-together',
+      message:
+        'Wir machen das so: Ich zeige dir, wie ich meine Homepage programmiere und du kannst dann in jedem Schritt deine eigene Homepage entwickeln. 🙃💪',
+      trigger: 'explain-language-basic',
+      delay: 3000,
+    },
+    {
+      id: 'explain-language-basic',
+      message:
+        'Zuerst erkläre ich dir ganz grob, wie die Sprache funktioniert. ',
+      trigger: 'explain-language-basic-tags',
+      delay: 1000,
+    },
+    {
+      id: 'explain-language-basic-tags',
+      message:
+        'Immer, wenn wir was definieren wollen, müssen wir sagen, wo es anfängt und wo es aufhört. Dafür nutzen wir Tags. 🤖',
+      trigger: 'explain-language-basic-tags-language',
+      delay: 2000,
+    },
+    {
+      id: 'explain-language-basic-tags-language',
+      message:
+        'Nein, das sind keine Wochentage. Das spricht man aus wie TÄÄG! Das ist sowas wie ein Name. Am Anfang sieht das dann so aus:',
+      trigger: 'code-explain-language-basic-tags',
+      delay: 2000,
+    },
+    {
+      id: 'code-explain-language-basic-tags',
+      trigger: 'ask-tag-input',
+      component: <CodeBlock content={`<tag>`} />,
+      delay: 1000,
+    },
+    {
+      id: 'ask-tag-input',
+      message:
+        'Such direkt mal die Zeichen auf deiner Tastatur und tippe <tag> selbst ein :)',
+      trigger: 'enter-tag-input',
+      delay: 1000,
+    },
+    {
+      id: 'enter-tag-input',
+      user: true,
+      trigger: (props: TriggerProps) => {
+        console.log(props.value);
+        if (props.value === '<tag>') {
+          return 'enter-tag-input-true';
+        } else {
+          return 'enter-tag-input-false';
+        }
+      },
+    },
+    {
+      id: 'enter-tag-input-true',
+      message: 'Das klappt ja super!',
+      trigger: 'code-explain-language-basic-close-tags',
+    },
+    {
+      id: 'enter-tag-input-false',
+      message: 'Das stimmt noch nicht ganz, versuche es nochmal!',
+      trigger: 'enter-tag-input',
+    },
+    {
+      id: 'code-explain-language-basic-close-tags',
+      message:
+        'Am Ende eines Tags müssen wir dann sagen, dass es vorbei ist. 🙅️ Das machen wir so:',
+      trigger: 'code-explain-language-basic-close-tags-code',
+      delay: 2000,
+    },
+    {
+      id: 'code-explain-language-basic-close-tags-code',
+      trigger: 'explain-slash',
+      component: <CodeBlock content={`</tag>`} />,
+    },
+    {
+      id: 'explain-slash',
+      message:
+        'Es ist ganz wichtig, dass wir hier den schrägen Strich nicht vergessen. Dann geht nämlich alles kaputt. 🙇 Gib es jetzt selbst ein.',
+      trigger: 'enter-slash',
+      delay: 3000,
+    },
+    {
+      id: 'enter-slash',
+      user: true,
+      trigger: 'only-smartphone',
+      validate:
+        'Überüfe, ob user ein Slash eingibt; wenn true dann trigger= enter-slash-true; wenn false dann trigger=enter-slash-false ',
+      delay: 2000,
+    },
+    {
+      id: 'enter-slash-false',
+      message: 'Das stimmt noch nicht ganz, versuche es nochmal',
+      trigger: 'enter-slash',
+    },
+    {
+      id: 'enter-slash-true',
+      message:
+        'Sehr gut! Jetzt haben wir erstmal genug Theorie gehabt, jetzt geht es los! 💪',
+      // trigger: "hast-du-lust-loszulegen"
+      delay: 1000,
+      end: true,
+    },
+  ];
+
+  const stepsOld = [
     {
       id: 'start',
       message: `Moin, was möchtest du machen?`,
@@ -144,20 +293,15 @@ export function ProgrammingChatBot() {
     {
       id: 'enter-code',
       user: true,
-      trigger: 'set-code',
-    },
-    {
-      id: 'set-code',
-      message: (args: MessageArgs) => {
-        dispatch({ type: 'setCode', value: args.previousValue });
-        return `Code saved!`;
+      trigger: (props: TriggerProps) => {
+        dispatch({ type: 'setCode', value: props.value });
+        return 'show-code-block';
       },
-      trigger: 'show-code-block',
     },
     {
       id: 'show-code-block',
       asMessage: true,
-      component: <CodeBlock content={`<tag>`}  />,
+      component: <CodeBlock content={`<tag>`} />,
       trigger: 'profile',
     },
   ];
