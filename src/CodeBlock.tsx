1304da856181c1e3254188549ddf2b96080cf677
André Kovac <me@andrekovac.com>
2020-03-22 11:46:35 +0100

    Run style fix

--- src/CodeBlock.tsx
+++ src/CodeBlock.tsx
@@ -12,28 +12,29 @@ export const CodeBlock = (props: { content?: string }) => {
   return (
     <div
       style={{
-        fontFamily: 'Courier New, Courier, Lucida Sans Typewriter, Lucida Typewriter, monospace',
+        fontFamily:
+          'Courier New, Courier, Lucida Sans Typewriter, Lucida Typewriter, monospace',
         backgroundColor: '#4d0000',
         padding: 10,
         borderRadius: 5,
       }}
     >
       <HeaderBar />
-      {props.content ? props.content : (code || `<NO CODE>`)}
+      {props.content ? props.content : code || `<NO CODE>`}
     </div>
-  )
-}
+  );
+};
 
 const Circle = (props: { color: string }) => (
   <div
-          style={{
-            width: 10,
-            height: 10,
-            backgroundColor: props.color,
-            borderRadius: 50,
-            marginRight: 5,
-          }}
-        />
+    style={{
+      width: 10,
+      height: 10,
+      backgroundColor: props.color,
+      borderRadius: 50,
+      marginRight: 5,
+    }}
+  />
 );
 
 const HeaderBar = () => (
@@ -42,17 +43,18 @@ const HeaderBar = () => (
       style={{
         paddingBottom: 10,
         display: 'flex',
-        justifyContent: 'flex-start'
+        justifyContent: 'flex-start',
       }}
     >
       <Circle color="red" />
       <Circle color="yellow" />
       <Circle color="lightgreen" />
     </div>
-    <div style={{
-      borderBottom: '0.5px solid grey',
-      marginBottom: 10,
-    }}>
-    </div>
+    <div
+      style={{
+        borderBottom: '0.5px solid grey',
+        marginBottom: 10,
+      }}
+    ></div>
   </>
 );
